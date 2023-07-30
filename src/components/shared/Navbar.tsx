f236a9d2d74e076f26b92eb0ad8ca96c9875f8de
= <=>
2023-07-30 09:54:48 +0600

    mobile navbar added

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,8 +1,27 @@
-import { Box, Flex, IconButton, Image, Input } from '@chakra-ui/react'
-
+import { HamburgerIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  Flex,
+  Image,
+  Input,
+  VStack,
+  useDisclosure
+} from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import logo from '../../assets/img/logo.png'
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const NavItems = (
+    <>
+      <Link to={'/'}>Home</Link>
+      <Link to={'/'}>Sign up</Link>
+      <Link to={'/'}>All Books</Link>
+    </>
+  )
   return (
     <Box
       color={'#ffffff'}
@@ -16,7 +35,7 @@ const Navbar = () => {
         <Image filter={'transparent'} w={12} src={logo} />
         <h4>My Book</h4>
       </Flex>
-      <Box flex={2}>
+      <Box flex={2} display={{ base: 'none', md: 'block' }}>
         <Input placeholder='Search books' />
       </Box>
       <Flex
@@ -25,11 +44,20 @@ const Navbar = () => {
         gap={5}
         justifyItems={'end'}
         justifyContent={'flex-end'}>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/'}>Sign up</Link>
-        <Link to={'/'}>All Books</Link>
+        {NavItems}
       </Flex>
-      <IconButton aria-label='nav' ml={5} />
+      <HamburgerIcon display={{ base: 'block', md: 'none' }} onClick={onOpen} />
+      <Drawer placement={'top'} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent bg={'blue.900'}>
+          <DrawerBody pb={3}>
+            <Input placeholder='Search books' my={5} />
+            <VStack color={'#ffffff'} justifyItems={'flex-start'}>
+              {NavItems}
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
     </Box>
   )
 }
