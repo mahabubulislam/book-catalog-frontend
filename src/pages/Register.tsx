af8ee561a49ca82d057344be4abcff5a0cef904b
mahabubulislam <mahabub2k1@gmail.com>
2023-08-03 18:00:06 +0600

    register page added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -8,7 +8,7 @@ import {
 } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 
-const Login = () => {
+const Register = () => {
   return (
     <Container h={'80vh'} alignItems={'center'} display={'flex'}>
       <FormControl isRequired boxShadow={'xl'} p={10} rounded={'md'}>
@@ -18,24 +18,26 @@ const Login = () => {
           fontWeight={'bold'}
           fontSize={'3xl'}
           mb={5}>
-          Login
+          Register
         </Text>
         <FormLabel>Email</FormLabel>
         <Input placeholder='example@mail.com' type='email' />
         <FormLabel my={3}>Password</FormLabel>
         <Input placeholder='********' type='password' />
+        <FormLabel my={3}>Mobile</FormLabel>
+        <Input placeholder='0146354344' type='number' />
         <Text as={'p'} mt={5}>
-          Don't have an account ?{' '}
-          <Text color={'teal'} as={Link} to={'/register'}>
-            Register here
+          Already have an account ?{' '}
+          <Text color={'teal'} as={Link} to={'/login'}>
+            Login here
           </Text>
         </Text>
         <Button mt={4} colorScheme='teal' type='submit'>
-          Login
+          Register
         </Button>
       </FormControl>
     </Container>
   )
 }
 
-export default Login
+export default Register
